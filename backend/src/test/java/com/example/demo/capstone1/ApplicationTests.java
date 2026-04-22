664ac1ea1fc67093f4879b5c231728f43727f083
kwangmin <rhkdals2585@naver.com>
2026-04-22 11:18:35 +0900

    chore: initialize capstone2026-cicd project

---
+++ backend/src/test/java/com/example/demo/capstone1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.capstone1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
